13339dc68c572c422417253608882465d5f12a02
coma <tamagomapon@gmail.com>
2023-06-16 16:14:42 +0000

    [finish]jQueryでの画像のポップアップ表示

---
+++ app.js
@@ -0,0 +1,14 @@
+// 画像拡大表示用
+const modal = $('#modal-container');
+const img = modal.find('img');
+
+$('img.popup').each(function(index) {
+  $(this).click(function() {
+    img.attr('src', $(this).attr('src'));
+    modal.show();
+  })
+});
+
+modal.click(function() {
+  $(this).hide();
+});
